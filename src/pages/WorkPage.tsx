87de2efb3eaa1c4ad59a98de2ce0f9e86bf9f55a
Alexey Zakhlestin <indeyets@gmail.com>
2023-02-07 20:04:01 +0000

    Add links

--- src/pages/WorkPage.tsx
+++ src/pages/WorkPage.tsx
@@ -1,5 +1,6 @@
 import { JSXElement, Suspense, Show } from "solid-js";
 import { useRouteData } from "solid-start";
+import { A } from "@solidjs/router";
 
 const nl2p = (page: string): JSXElement =>
   page.split("\n").map((line) => <p>{line}</p>);
@@ -14,6 +15,10 @@ const WorkPage = () => {
         <p>{data.work()?.description || ""}</p>
         <div>{nl2p(data.work()?.pages[0] || "")}</div>
       </div>
+      <A href="/s/test">Page 1</A>
+      <br />
+      <A href="/s/test?page=2">Page 2</A>
+      <br />
     </Suspense>
   );
 };
